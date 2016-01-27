fb855a38c01a0179b14f826e9a23a30091456826
evanwchen <evanwchen@gmail.com>
2016-01-27 15:12:29 -0800

    Complete 6ees6ees6ees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,6 +1,20 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee{
+  constructor(prop){
+    super(prop)
+    this.age = 40
+    this.job = 'gamble'
+    this.canFly = false
+    this.color = 'grey'
+  }
+  forage(){
+    return "I am too old, let me play cards instead"
+  }
+  gamble(treasure){
+    this.treasureChest.push(treasure)
+  }
+
   // TODO..
 };
 
